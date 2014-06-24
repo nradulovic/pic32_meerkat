f6b93a0696adcf2df50840728f6c484f846e5457
Nenad Radulović <nenad.b.radulovic@gmail.com>
2014-06-25 00:35:13 +0200

    Serial EPA implementation

---
+++ application/include/epa_serial.h
@@ -0,0 +1,78 @@
+/*
+ * File:   epa_radio.h
+ * Author: nenad
+ *
+ * Created on April 27, 2014, 1:12 PM
+ */
+/*----------------------------------------------------------------------------*
+ * NOTE: This file must be included only through events.h file
+ *----------------------------------------------------------------------------*/
+
+#ifndef EPA_SERIAL_H
+#define EPA_SERIAL_H
+
+/*=========================================================  INCLUDE FILES  ==*/
+
+#include <stdint.h>
+
+#include "eds/epa.h"
+#include "driver/uart.h"
+
+/*===============================================================  MACRO's  ==*/
+
+#define CONFIG_SERIAL_PRIORITY           30
+#define CONFIG_SERIAL_QUEUE_SIZE         10
+#define CONFIG_SERIAL_EVENT_BASE         1500
+
+/*------------------------------------------------------  C++ extern begin  --*/
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+/*============================================================  DATA TYPES  ==*/
+
+enum serialEvents {
+    EVT_SERIAL_OPEN = CONFIG_SERIAL_EVENT_BASE,
+    EVT_SERIAL_CLIENT,
+    EVT_SERIAL_PACKET,
+    EVT_SERIAL_RECV,
+    EVT_SERIAL_SEND_CANCEL,
+    EVT_SERIAL_RECV_CANCEL
+};
+
+struct evtSerialOpen {
+    struct esEvent      event;
+    struct uartConfig   config;
+};
+
+struct evtSerialClient {
+    struct esEvent      event;
+    esEpa *             client;
+};
+
+struct evtSerialPacket {
+    struct esEvent      event;
+    const void *        data;
+    size_t              size;
+};
+
+/*======================================================  GLOBAL VARIABLES  ==*/
+
+extern const struct esEpaDefine SerialEpa;
+extern const struct esSmDefine  SerialSm;
+extern struct esEpa *   Serial1;
+extern struct esEpa *   Serial2;
+
+/*===================================================  FUNCTION PROTOTYPES  ==*/
+
+/*--------------------------------------------------------  C++ extern end  --*/
+#ifdef __cplusplus
+}
+#endif
+
+/*================================*//** @cond *//*==  CONFIGURATION ERRORS  ==*/
+/** @endcond *//** @} *//** @} *//*********************************************
+ * END of epa_serial.h
+ ******************************************************************************/
+#endif /* EPA_SERIAL_H */
+
---
+++ application/source/epa_serial.c
@@ -0,0 +1,167 @@
+/*
+ * File:   epa_radio.c
+ * Author: nenad
+ *
+ * Created on April 27, 2014, 1:14 PM
+ */
+
+/*=========================================================  INCLUDE FILES  ==*/
+
+#include "epa_serial.h"
+#include "driver/uart.h"
+#include "app_timer.h"
+
+/*=========================================================  LOCAL MACRO's  ==*/
+
+#define SERIAL_TABLE(entry)                                                     \
+    entry(stateInit,                TOP)                                        \
+    entry(stateIdle,                TOP)
+
+/*======================================================  LOCAL DATA TYPES  ==*/
+
+enum serialStateId {
+    ES_STATE_ID_INIT(SERIAL_TABLE)
+};
+
+enum localEvents {
+    EVT_UART_RX_RESPONSE_ = ES_EVENT_LOCAL_ID,
+    EVT_UART_ERROR_
+};
+
+struct uartEvent_ {
+    struct esEvent      header;
+    size_t              size;
+};
+
+struct wspace {
+    struct uartHandle   uart;
+    struct esEpa *      client;
+    char                replyBuffer[256];
+};
+
+/*=============================================  LOCAL FUNCTION PROTOTYPES  ==*/
+
+static esAction stateInit           (struct wspace *, const esEvent *);
+static esAction stateIdle           (struct wspace *, const esEvent *);
+
+static size_t reader(struct uartHandle *, enum uartError, void *, size_t);
+
+/*=======================================================  LOCAL VARIABLES  ==*/
+
+static const ES_MODULE_INFO_CREATE("Serial", "Serial management", "Nenad Radulovic");
+
+static const esSmTable SerialTable[] = ES_STATE_TABLE_INIT(SERIAL_TABLE);
+
+/*======================================================  GLOBAL VARIABLES  ==*/
+
+const struct esEpaDefine SerialEpa = ES_EPA_DEFINE(
+    "Serial",
+    CONFIG_SERIAL_PRIORITY,
+    CONFIG_SERIAL_QUEUE_SIZE);
+
+const struct esSmDefine SerialSm = ES_SM_DEFINE(
+    SerialTable,
+    sizeof(struct wspace),
+    stateInit);
+
+struct esEpa *          Serial1;
+struct esEpa *          Serial2;
+
+/*============================================  LOCAL FUNCTION DEFINITIONS  ==*/
+
+static esAction stateInit(struct wspace * wspace, const esEvent * event) {
+    switch (event->id) {
+        case ES_INIT : {
+            wspace->client = NULL;
+
+            return (ES_STATE_HANDLED());
+        }
+        case EVT_SERIAL_OPEN : {
+            uartOpen(&wspace->uart, &((struct evtSerialOpen *)event)->config);
+            uartSetClient(&wspace->uart, esEdsGetCurrent());
+            uartSetReader(&wspace->uart, reader);
+
+            return (ES_STATE_TRANSITION(stateIdle));
+        }
+        default : {
+
+            return (ES_STATE_IGNORED());
+        }
+    }
+}
+
+static esAction stateIdle(struct wspace * wspace, const esEvent * event) {
+    switch (event->id) {
+        case ES_ENTRY : {
+            uartReadStart(&wspace->uart, &wspace->replyBuffer, sizeof(wspace->replyBuffer),
+                ES_VTMR_TIME_TO_TICK_MS(20));
+
+            return (ES_STATE_HANDLED());
+        }
+        case EVT_SERIAL_CLIENT : {
+            wspace->client = ((const struct evtSerialClient *)event)->client;
+
+            return (ES_STATE_HANDLED());
+        }
+        case EVT_SERIAL_PACKET : {
+
+            while (uartWriteStart(&wspace->uart, ((const struct evtSerialPacket *)event)->data,
+                ((const struct evtSerialPacket *)event)->size) == UART_ERROR_BUSY);
+
+            return (ES_STATE_HANDLED());
+        }
+        case EVT_UART_RX_RESPONSE_ : {
+
+            if (wspace->client != NULL) {
+                struct esEvent *    packet;
+                esError             error;
+
+                error = esEventCreate(sizeof(struct evtSerialPacket), EVT_SERIAL_PACKET, &packet);
+
+                if (!error) {
+                    ((struct evtSerialPacket *)packet)->data = &wspace->replyBuffer;
+                    ((struct evtSerialPacket *)packet)->size =
+                        ((const struct uartEvent_ *)event)->size;
+                     esEpaSendEvent(wspace->client, packet);
+                }
+            }
+            uartReadStart(&wspace->uart, &wspace->replyBuffer, sizeof(wspace->replyBuffer),
+                ES_VTMR_TIME_TO_TICK_MS(20));
+
+            return (ES_STATE_HANDLED());
+        }
+        default : {
+
+            return (ES_STATE_IGNORED());
+        }
+    }
+}
+
+static size_t reader(struct uartHandle * handle, enum uartError uartError, void * data, size_t size) {
+    struct uartEvent_ * uartEvt;
+    uint16_t            id;
+    esError             error;
+
+    (void)data;
+    
+    if ((uartError == UART_ERROR_NONE) || (uartError == UART_ERROR_TIMEOUT)) {
+        id = EVT_UART_RX_RESPONSE_;
+    } else {
+        id = EVT_UART_ERROR_;
+    }
+    ES_ENSURE(error = esEventCreateI(sizeof(struct uartEvent_), id, (esEvent **)&uartEvt));
+
+    if (!error) {
+        uartEvt->size = size;
+        esEpaSendAheadEventI(handle->epa, (esEvent *)uartEvt);
+    }
+
+    return (0u);
+}
+
+/*===================================  GLOBAL PRIVATE FUNCTION DEFINITIONS  ==*/
+/*====================================  GLOBAL PUBLIC FUNCTION DEFINITIONS  ==*/
+/*================================*//** @cond *//*==  CONFIGURATION ERRORS  ==*/
+/** @endcond *//** @} *//******************************************************
+ * END of epa_serial.c
+ ******************************************************************************/
